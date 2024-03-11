b119b75996f4c1a2e68aa28649f904b638daeb3c
EzDev <manhthai2003ez@gmail.com>
2024-03-11 08:31:37 +0700

    build: re set up project

--- app.js
+++ app.js
@@ -1,65 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
+const db = require("./configs/database");
+const apiResponseMiddleware = require("./middlewares/response");
+const errorMiddleware = require("./middlewares/error");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var apiRouter = require("./routes/api");
 
 var app = express();
 
-// #config
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// connect database
+db.connectDb();
 
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//# middlewares
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(session({
-    secret: 'tranghtmlalalallala123aptx4869',
+app.use(express.static(path.join(__dirname, "public")));
+app.use(apiResponseMiddleware());
+app.use(
+  session({
+    secret: "tranghtmlalalallala123aptx4869",
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
     // ,cookie: { secure: true }
-}))
+  })
+);
 
-// #routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+//# routes
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api", apiRouter);
 
-// #middlewares
-app.use(function (req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-
-    if (req.originalUrl.indexOf('/api') == 0) {
-        res.json(
-            {
-                status: 0,
-                msg: err.message
-            }
-        );
-    } else {
-        res.render('error');
-    }
-
-});
+//# middlewares
+app.use(errorMiddleware.notFound);
+app.use(errorMiddleware.errorHanlder);
 
 module.exports = app;
-
--- configs/database.js
+++ configs/database.js
@@ -1,9 +1,45 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/MD12_Haponla")
-.catch((err) => {
-  console.log("Loi ket noi CSDL");
-  console.log(err);
+// mongoose.connect("mongodb://127.0.0.1:27017/MD12_Haponla")
+// .catch((err) => {
+//   console.log("Loi ket noi CSDL");
+//   console.log(err);
+// });
+
+const dbConfig = {
+  url: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/MD12_Haponla",
+  options: {
+    // useNewUrlParser: true, useUnifiedTopology: true
+  },
+};
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(dbConfig.url, dbConfig.options);
+  } catch (error) {
+    console.log(`MongoDB error: ${JSON.stringify(error)}`);
+    // process.exit(0);
+    process.exit(1);
+  }
+};
+
+const dbConnection = mongoose.connection;
+
+dbConnection.on("connected", function () {
+  console.log(`MongoDB connected: ${this.name}`);
 });
 
-module.exports = { mongoose };
+dbConnection.on("disconnected", function () {
+  console.log(`MongoDB disconnected: ${this.name}`);
+});
+
+dbConnection.on("error", function (error) {
+  console.log(`MongoDB error: ${JSON.stringify(error)}`);
+});
+
+process.on("SIGINT", async function (error) {
+  await dbConnection.close();
+  process.exit(0);
+});
+
+module.exports = { mongoose, connectDb };
