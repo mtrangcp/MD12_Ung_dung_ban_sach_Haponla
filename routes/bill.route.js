bbcc80ace1d1104d1076a81e3c7462621e5a2049
mtrangcp <haminhtrang545@gmail.com>
2024-03-11 21:52:32 +0700

    chinh sua

--- routes/bill.route.js
+++ routes/bill.route.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const controller = require("../controllers/api/bill.controller");
 
 router.get("/", controller.getAll);
-router.post("/", controller.add);
+router.post("/add", controller.add);
 router.get("/:id", controller.get);
-router.delete("/:id", controller.remove);
+router.delete("/del/:id", controller.remove);
 router.patch("/:id", controller.set);
 
 module.exports = router;
