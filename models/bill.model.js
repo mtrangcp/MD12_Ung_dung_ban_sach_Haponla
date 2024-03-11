7c85a56e77dd85892b300a83805d7e74667fef54
mtrangcp <haminhtrang545@gmail.com>
2024-03-11 22:19:44 +0700

    chinh sua

--- models/bill.model.js
+++ models/bill.model.js
@@ -1,6 +1,3 @@
-/**
- * dev: ManhThai
- */
 const { Schema, model } = require("mongoose");
 const ObjectId = Schema.ObjectId;
 const { BookModel } = require("./book.model");
@@ -8,16 +5,8 @@ const { userModel, addressModel, discountModel } = require("./bookStore.model");
 
 // bill item
 const BillItemSchema = new Schema({
-  quantity: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
-  id_book: {
-    type: ObjectId,
-    ref: BookModel.modelName,
-    required: true,
-  },
+  quantity: { type: Number, min: 0, required: true },
+  id_book: { type: ObjectId, ref: BookModel.modelName, required: true },
 });
 
 const BillItemModel = model("bill_item", BillItemSchema);
@@ -27,26 +16,15 @@ const BillSchema = new Schema({
   status: {
     /*
         0: canceled
-        1: shipping
-        2: received
+        1: wait
+        2: shipping
+        3: received
     */
-    type: Number,
-    min: 0,
-    max: 2,
-    default: 1,
+    type: Number, min: 0, max: 3, default: 1
   },
-  temp_price: {
-    type: Number,
-    min: 0,
-  },
-  real_price: {
-    type: Number,
-    min: 0,
-  },
-  create_at: {
-    type: Date,
-    default: Date.now,
-  },
+  temp_price: { type: Number, min: 0 },
+  real_price: { type: Number, min: 0 },
+  create_at: { type: Date, default: Date.now },
   detail: [
     {
       type: ObjectId,
@@ -54,20 +32,9 @@ const BillSchema = new Schema({
       required: true,
     },
   ],
-  id_user: {
-    type: ObjectId,
-    ref: userModel.modelName,
-    required: true,
-  },
-  id_address: {
-    type: ObjectId,
-    ref: addressModel.modelName,
-    required: true,
-  },
-  id_discount: {
-    type: ObjectId,
-    ref: discountModel.modelName,
-  },
+  id_user: { type: ObjectId, ref: userModel.modelName, required: true },
+  id_address: { type: ObjectId, ref: addressModel.modelName, required: true },
+  id_discount: { type: ObjectId, ref: discountModel.modelName },
 });
 
 const BillModel = model("bill", BillSchema);
