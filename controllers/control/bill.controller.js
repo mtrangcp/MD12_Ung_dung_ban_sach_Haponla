aadd237f5a9d141702fcbd559bd565c4b2eee954
mtrangcp <haminhtrang545@gmail.com>
2024-03-19 14:44:52 +0700

    chinh sua

--- controllers/control/bill.controller.js
+++ controllers/control/bill.controller.js
@@ -9,12 +9,14 @@ exports.getListBill = async (req, res, next) => {
 }
 
 exports.getBillUsername = async (req, res, next) => {
+    let idU = req.params.idUser;
+    console.log("idUser: " + idU);
     try {
-        let idU = req.params.idU;
+
         let dk_loc = { id_user: idU };
         console.log("----dk loc:" + dk_loc);
 
-        var list = await myModel.BillModel.find(dk_loc).populate('id_user').populate('id_address').populate('id_discount').populate('detail');
+        var list = await myModel.BillModel.find(dk_loc).populate('id_address').populate('id_discount');
         var soluong = list.length;
     } catch (error) {
         console.log(error);
@@ -23,7 +25,75 @@ exports.getBillUsername = async (req, res, next) => {
     res.render('bills/listBillUser', { listBillUser: list, soluong: soluong });
 }
 
+exports.getPriceThapCao = async (req, res, next) => {
+    let idU = req.params.idUser;
+    console.log("idUser: " + idU);
+    try {
 
+        let dk_loc = { id_user: idU };
+        console.log("----dk loc:" + dk_loc);
+
+        var list = await myModel.BillModel.find(dk_loc).sort({ real_price: -1 }).populate('id_address').populate('id_discount');
+        var soluong = list.length;
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.render('bills/listBillUser', { listBillUser: list, soluong: soluong });
+}
+exports.getPriceCaoThap = async (req, res, next) => {
+    let idU = req.params.idUser;
+    console.log("idUser: " + idU);
+    try {
+
+        let dk_loc = { id_user: idU };
+        console.log("----dk loc:" + dk_loc);
+
+        var list = await myModel.BillModel.find(dk_loc).sort({ real_price: 1 }).populate('id_address').populate('id_discount');
+        var soluong = list.length;
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.render('bills/listBillUser', { listBillUser: list, soluong: soluong });
+}
+
+exports.changeStatus = async (req, res, next) => {
+    let msg = "";
+    try {
+        let _id = req.params.id;
+        console.log("idUser: " + _id);
+        let dk_loc = { _id: _id };
+        console.log("----dk loc:" + dk_loc);
+
+        var objBill = await myModel.BillModel.find(dk_loc).populate('id_user').populate('id_address').populate('id_discount').populate('detail');
+        console.log(objBill);
+        if (req.method == 'POST') {
+            let objBill = new myModel.spModel();
+
+            if (typeof (req.body.TrangThai != 'undefined')) {
+                objBill.status = req.body.TrangThai;
+            }
+
+            objBill._id = _id;
+
+            try {
+                await myModel.spModel.findByIdAndUpdate({ _id: _id }, objBill);
+                msg = 'Sửa thành công!'
+                console.log(objBill);
+
+            } catch (err) {
+                msg = 'Lỗi: ' + err;
+                console.log(err);
+            }
+
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.render('bills/changeStatus', { objBill: objBill, msg: msg, cre: objBill.create_at });
+}
 
 
 
--- routes/bill.js
+++ routes/bill.js
@@ -11,8 +11,20 @@ router.use((req, res, next) => {
 
 router.get('/', check_login.yeu_cau_login, billCtrl.getListBill);
 
-router.get('/:idU', billCtrl.getBillUsername);
-router.post('/:idU', billCtrl.getBillUsername);
+router.get('/:idUser', billCtrl.getBillUsername);
+router.post('/:idUser', billCtrl.getBillUsername);
+
+router.get('/status/:id', billCtrl.changeStatus);
+router.post('/status/:id', billCtrl.changeStatus);
+
+router.get('/thapCao', billCtrl.getPriceThapCao);
+router.post('/thapCao', billCtrl.getPriceThapCao);
+router.get('/caoThap', billCtrl.getPriceCaoThap);
+router.post('/caoThap', billCtrl.getPriceCaoThap);
+
+
+
+
 
 
 module.exports = router;
