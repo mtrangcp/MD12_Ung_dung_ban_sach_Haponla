8df7fa0392d5dbed6f39ef06156aacd1823bb402
mtrangcp <haminhtrang545@gmail.com>
2024-04-20 01:52:25 +0700

    dieu huong thong ke

--- controllers/control/thongKe.controller.js
+++ controllers/control/thongKe.controller.js
@@ -1,7 +1,15 @@
 var myModel = require("../../models/bill.model");
 var BookModel = require("../../models/book");
 
+exports.getThongKe = async (req, res, next) => {
+    var list = await myModel.BillModel.find()
+        .populate("id_address")
+        .populate("id_discount")
+        .populate("id_user");
+    var soluong = list.length;
 
+    res.render("bills/thongKe", { listBill: list, soluong: soluong });
+};
 
 
 
