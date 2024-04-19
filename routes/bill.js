e7192dd0ebc1dbd8eec8d92b837117e063b3ea4a
mtrangcp <haminhtrang545@gmail.com>
2024-04-19 17:41:16 +0700

    chinh sua ok

--- routes/bill.js
+++ routes/bill.js
@@ -14,11 +14,21 @@ router.get('/', check_login.yeu_cau_login, billCtrl.getListBill);
 router.get('/:idUser', billCtrl.getBillUsername);
 router.post('/:idUser', billCtrl.getBillUsername);
 
-router.get('/status/:id', billCtrl.changeStatus);
-router.post('/status/:id', billCtrl.changeStatus);
+router.get('/changeStatus/:id', billCtrl.changeStatus);
+router.post('/changeStatus/:id', billCtrl.changeStatus);
 
 router.get('/chitiet/:idBill', billCtrl.chitietBill);
 router.post('/chitiet/:idBill', billCtrl.chitietBill);
+
+router.get('/sx/tongThapCao', billCtrl.getTongThapCao);
+router.post('/sx/tongThapCao', billCtrl.getTongThapCao);
+router.get('/sx/tongCaoThap', billCtrl.getTongCaoThap);
+router.post('/sx/tongCaoThap', billCtrl.getTongCaoThap);
+
+router.get('/sx/tgMoi', billCtrl.getBillTgMoi);
+router.post('/sx/tgMoi', billCtrl.getBillTgMoi);
+router.get('/sx/tgCu', billCtrl.getBillTgCu);
+router.post('/sx/tgCu', billCtrl.getBillTgCu);
 
 router.get('/tienThapCao/:idUser', billCtrl.getBillUserTienThapCao);
 router.post('/tienThapCao/:idUser', billCtrl.getBillUserTienThapCao);
@@ -41,26 +51,8 @@ router.post('/3/:idUser', billCtrl.getBillUser3);
 router.get('/4/:idUser', billCtrl.getBillUser4);
 router.post('/4/:idUser', billCtrl.getBillUser4);
 
-router.get('/tienThapCao', billCtrl.getBillUserTienThapCao);
-router.post('/tienThapCao', billCtrl.getBillUserTienThapCao);
-router.get('/tienCaoThap', billCtrl.getBillUserTienCaoThap);
-router.post('/tienCaoThap', billCtrl.getBillUserTienCaoThap);
-
-router.get('/tgMoi', billCtrl.getBillUserTgMoi);
-router.post('/tgMoi', billCtrl.getBillUserTgMoi);
-router.get('/tgCu', billCtrl.getBillUserTgCu);
-router.post('/tgCu', billCtrl.getBillUserTgCu);
 
 router.get('/status/:status', billCtrl.getBillWithStatus);
 router.post('/status/:status', billCtrl.getBillWithStatus);
-// router.get('/1', billCtrl.getBillUser1);
-// router.post('/1', billCtrl.getBillUser1);
-// router.get('/2', billCtrl.getBillUser2);
-// router.post('/2', billCtrl.getBillUser2);
-// router.get('/3', billCtrl.getBillUser3);
-// router.post('/3', billCtrl.getBillUser3);
-// router.get('/4', billCtrl.getBillUser4);
-// router.post('/4', billCtrl.getBillUser4);
-
 
 module.exports = router;
